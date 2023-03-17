974dbfdee3f89505c23c7c2a717cffac500432c2
LeiLi <Leo321088@gmail.com>
2023-03-17 21:12:30 +0100

    Hotfix 2th modify

--- src/main/java/com/atguigu/gitTest/GitTest01.java
+++ src/main/java/com/atguigu/gitTest/GitTest01.java
@@ -4,6 +4,8 @@ public class GitTest01  {
     public static void main(String[] args) {
         System.out.println("Git");
         System.out.println("Git02");
+        System.out.println("Git03");
+        System.out.println("Git04");
         System.out.println("Hotfix");
     }
 }
